6e95d7e9d221e06ab4614d9dfd99ba1c94900586
Sanjivani Singh <sanjivanisngh@gmail.com>
2025-07-26 22:54:01 +0530

    done server

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,11 +1,11 @@
 const multer = require("multer");
 
 const path = require("path");
-const uploadPath = path.join(__dirname, "..", "uploads");
+// const uploadPath = path.join(__dirname, "..", "uploads");
 
 const storage = multer.diskStorage({
     destination : function (req, file, cb) {
-        cb(null, uploadPath)
+        cb(null, "/tmp")
     },
     filename : function(req, file, cb){
         const fileName = Date.now() + path.extname(file.originalname)
