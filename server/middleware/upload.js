bd745702f0579ca9a035893a38c89a2f49900a89
Sanjivani Singh <sanjivanisngh@gmail.com>
2025-07-26 20:12:21 +0530

    changes resolved

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 
-const path = requuire("path");
+const path = require("path");
 
 const storage = multer.diskStorage({
     destination : function (req, file, cb) {
